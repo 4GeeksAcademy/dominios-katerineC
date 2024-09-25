5ebcf26a9288e85e7fbd806fe487ce6b3b7770ae
Katerine Céspedes <katerinecesbal@hotmail.com>
2024-09-25 03:20:55 +0000

    dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".uy", ".es", ".ar"];
+
+let dominios = [];
+
+pronoun.forEach((start) => {
+  adj.forEach((mid) => {
+    noun.forEach((end) => {
+      extension.forEach((ext) => {
+        let domain = `${start}${mid}${end}${ext}`;
+
+        console.log(domain);
+
+        dominios.push(domain);
+      });
+    });
+  });
+});
+
+console.log(dominios);
